05d8158cd0902ced3a663f89c2412be3b6853c6e
Jose Padilla <jose.apadilla99@gmail.com>
2023-07-02 14:29:45 -0500

    feat: mouse tracker added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
+"use client";
+
+//possibly make the track a component to avoid the whole page being a client side item
+
+import { useEffect } from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 
 export default function Home() {
   //might need to add overflow: hidden for now scroll bar
+  useEffect(() => {
+    const track = document.getElementById("img_track");
+    if (track) {
+      const handleMouseDown = (e: MouseEvent) => {
+        track.dataset.mouseDown = e.clientX.toString();
+      };
+
+      window.addEventListener("mousedown", handleMouseDown);
+      console.log(track)
+      return () => {
+        window.removeEventListener("mousedown", handleMouseDown);
+      };
+    }
+  }, []);
+
   return (
     <main className={styles.main_wrapper}>
       <h1 className={styles.page_title}>Welcome.</h1>
@@ -14,12 +34,42 @@ export default function Home() {
           and volunteering as a Full-Stack Developer at Hack For LA.
         </p>
       </div>
-      <div className={styles.img_track}>
-        <Image src="/sampleImg.jpeg" height={200} width={300} alt="sample img" className={styles.image}/>
-        <Image src="/sampleImg.jpeg" height={200} width={300} alt="sample img" className={styles.image}/>
-        <Image src="/sampleImg.jpeg" height={200} width={300} alt="sample img" className={styles.image}/>
-        <Image src="/sampleImg.jpeg" height={200} width={300} alt="sample img" className={styles.image}/>
-        <Image src="/sampleImg.jpeg" height={200} width={300} alt="sample img" className={styles.image}/>
+      <div id="img_track" className={styles.img_track} data-mouse-down-at="0">
+        <Image
+          src="/sampleImg.jpeg"
+          height={200}
+          width={300}
+          alt="sample img"
+          className={styles.image}
+        />
+        <Image
+          src="/sampleImg.jpeg"
+          height={200}
+          width={300}
+          alt="sample img"
+          className={styles.image}
+        />
+        <Image
+          src="/sampleImg.jpeg"
+          height={200}
+          width={300}
+          alt="sample img"
+          className={styles.image}
+        />
+        <Image
+          src="/sampleImg.jpeg"
+          height={200}
+          width={300}
+          alt="sample img"
+          className={styles.image}
+        />
+        <Image
+          src="/sampleImg.jpeg"
+          height={200}
+          width={300}
+          alt="sample img"
+          className={styles.image}
+        />
       </div>
     </main>
   );
