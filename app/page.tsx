e107c03367926c441aa17dfa5771ad41e3bddbdc
Jose Padilla <jose.apadilla99@gmail.com>
2023-07-02 14:02:41 -0500

    feat: images on track setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
-import styles from './page.module.css'
+import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
+  //might need to add overflow: hidden for now scroll bar
   return (
     <main className={styles.main_wrapper}>
       <h1 className={styles.page_title}>Welcome.</h1>
       <div className={styles.page_intro}>
-      <p>My name is Jose Padilla. I'm a Full-Stack Developer in Houston, Texas. 
-      Programming is a true enjoyment for me since it accommodates my need of solving problems. 
-      Current I am sharpening my skills with porjects and volunteering as a full stack developer
-      at Hack For LA.
-      </p>
+        <p>
+          My name is Jose Padilla. I'm a Full-Stack Developer in Houston, Texas.
+          Programming is a true enjoyment of mine since it accommodates my need
+          of solving problems. Current I am sharpening my skills with projects
+          and volunteering as a Full-Stack Developer at Hack For LA.
+        </p>
+      </div>
+      <div className={styles.img_track}>
+        <Image src="/sampleImg.jpeg" height={200} width={300} alt="sample img" className={styles.image}/>
+        <Image src="/sampleImg.jpeg" height={200} width={300} alt="sample img" className={styles.image}/>
+        <Image src="/sampleImg.jpeg" height={200} width={300} alt="sample img" className={styles.image}/>
+        <Image src="/sampleImg.jpeg" height={200} width={300} alt="sample img" className={styles.image}/>
+        <Image src="/sampleImg.jpeg" height={200} width={300} alt="sample img" className={styles.image}/>
       </div>
     </main>
-  )
+  );
 }
