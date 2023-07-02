206f7376d7b3bb5207af5c9a078edd679750f137
Jose Padilla <jose.apadilla99@gmail.com>
2023-07-02 10:33:21 -0500

    feat: intro to page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,15 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main>
-      <h1> Hello world</h1>
+    <main className={styles.main_wrapper}>
+      <h1 className={styles.page_title}>Welcome.</h1>
+      <div className={styles.page_intro}>
+      <p>My name is Jose Padilla. I'm a Full-Stack Developer in Houston, Texas. 
+      Programming is a true enjoyment for me since it accommodates my need of solving problems. 
+      Current I am sharpening my skills with porjects and volunteering as a full stack developer
+      at Hack For LA.
+      </p>
+      </div>
     </main>
   )
 }
