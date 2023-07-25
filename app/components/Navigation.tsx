e514453a645fa88e1a3e88c2e983683d076f7c3e
Jose Padilla <jose.apadilla99@gmail.com>
2023-07-24 23:06:24 -0500

    feat: added social logos

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,28 +1,38 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const Navigation = () => {
   return (
     <header>
       <nav className="nav-wrapper">
-        <Link href="/">
-          <Image
-            src="/logo.png"
-            height={50}
-            width={150}
-            alt="Logo with the name Jose Padilla between brackets"
-          />
-        </Link>
-        <Link href="/portfolio" className="nav-link">
-          PORTFOLIO
-        </Link>
-        <Link href="/about" className="nav-link">
-          ABOUT
-        </Link>
-        <Link href="/resume" className="nav-link">
-          RESUME
-        </Link>
+        <div className="nav-left-items">
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              height={50}
+              width={150}
+              alt="Logo with the name Jose Padilla between brackets"
+            />
+          </Link>
+          <Link href="/portfolio" className="nav-link">
+            PORTFOLIO
+          </Link>
+          <Link href="/about" className="nav-link">
+            ABOUT
+          </Link>
+          <Link href="/resume" className="nav-link">
+            RESUME
+          </Link>
+        </div>
+        <div className="nav-right-items">
+          <FontAwesomeIcon icon={faLinkedin} />
+          <FontAwesomeIcon icon={faGithub} />
+          <FontAwesomeIcon icon={faTwitter}/>
+        </div>
       </nav>
     </header>
   );
