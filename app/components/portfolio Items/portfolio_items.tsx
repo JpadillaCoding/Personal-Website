5b0acef1a8d7c76d688c46eb38640fc3f276dddf
Jose Padilla <jose.apadilla99@gmail.com>
2023-08-02 21:43:29 -0500

    feat: portfolio card outter container is now responsive with inner container

--- app/components/portfolio Items/portfolio_items.tsx
+++ app/components/portfolio Items/portfolio_items.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
-import { relative } from "path";
 
-
-export default function PortfolioItem(props:any) {
-    const description:string = props.description
-    const image:string = props.src
-    const alt:string = props.alt
+export default function PortfolioItem(props: any) {
+  const description: string = props.description;
+  const image: string = props.src;
+  const alt: string = props.alt;
 
   return (
     <div className={styles.outer_wrapper}>
-        <div className={styles.inner_wrapper}>
-            <Image fill src={image} alt={alt}/>
-            <h2>{description}</h2>
-        </div>
+      <div className={styles.inner_wrapper}>
+          <Image fill src={image} alt={alt} />
+      </div>
+        <h2>{description}</h2>
     </div>
   );
 }
