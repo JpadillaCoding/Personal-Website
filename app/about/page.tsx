5a86a4a1e31c205aadedaf6f6d082ba7e8752ede
Jose Padilla <jose.apadilla99@gmail.com>
2023-08-09 19:01:04 -0500

    feat: skeleton for first left item

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,19 @@
+import styles from "./page.module.css";
+
 export default function About() {
-    return(
-        <div>
-            <h1>
-                About
-            </h1>
+  return (
+    <div className={styles.page_wrapper}>
+      <div className={styles.timeline}>
+        <div className={styles.content_container}>
+          <h2>First item..</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
