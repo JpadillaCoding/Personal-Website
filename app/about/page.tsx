161cba7ce1af37e086d2f6439db7bb4560635127
Jose Padilla <jose.apadilla99@gmail.com>
2023-08-13 17:06:24 -0500

    feat: right side configured

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,6 +17,20 @@ export default function About() {
           </div>
         </div>
       </div>
+      <div className={styles.timeline}>
+        <div className={`${styles.content_container}
+         ${styles.right}`}>
+          <div className={styles.content}>
+          <h2>Second item..</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
