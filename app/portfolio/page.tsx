1abdb6f2d44eccb08f81a9dd9c7025ee652419bf
Jose Padilla <jose.apadilla99@gmail.com>
2023-08-21 23:39:47 -0500

    style: color pallete for portfolio cards

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -17,14 +17,15 @@ export default function Portfolio() {
       </div>
       <div className="divider"></div>
       <div className={styles.portfolio_items_wrapper}>
-
         <PortfolioItem
           src={"/hackLa.png"}
           title={"HackForLA- Software Engineer volunteer"}
           alt={"screen shot of hack for LA homepage"}
           tech={"Typscript, React, Python, Redux, PostgresSQL, AWS, Docker"}
           link={"https://www.hackforla.org/"}
-          description={"Full stack experience by contributing to open source software to create civic-minded projects that benefit the communities of the greater LA area."}
+          description={
+            "Full stack experience by contributing to civic-minded projects that benefit the communities of the greater LA area."
+          }
         />
         <PortfolioItem
           src={"/curly.jpg"}
@@ -34,6 +35,9 @@ export default function Portfolio() {
             "React Native, Express.js, Redux, MongoDB, Google Cloud Services"
           }
           link={"https://github.com/JpadillaCoding/CurlSafe-Frontend"}
+          description={
+            "Mobile app that analyzes hair product ingreidents. Made for a forum that seeks to remove certain ingredients from their hair routine."
+          }
         />
         <PortfolioItem
           src={"/calorieTrakr.png"}
@@ -41,6 +45,7 @@ export default function Portfolio() {
           alt={"preview of calorie trakr homescreen"}
           tech={"Node.js, Express.js, React, JWT, MongoDB"}
           link={"https://github.com/JpadillaCoding/Front-end-MERN-Project"}
+          description={"Collabrative project that helps track micro and macro nutrient intake."}
         />
         <PortfolioItem
           src={"/nycApi.png"}
@@ -48,13 +53,15 @@ export default function Portfolio() {
           alt={"preview of NYC amenities api documentation"}
           tech={"Node.js, Express.js, MongoDB, REST API, Postman, Apidocs"}
           link={"https://github.com/JpadillaCoding/NYC-parks-and-amenities"}
+          description={"API that takes in data from NYC's OpenAPI to bundle amenities at parks in all 5 boroughs."}
         />
         <PortfolioItem
           src={"/spaceSnap.png"}
-          title={"SpaceSnap: NASA image search engine"}
+          title={"SpaceSnap: NASA image searcher"}
           alt={"preview of space snap home screen"}
           tech={"React, Bootstrap, API, Asynchronous programming "}
           link={"https://github.com/JpadillaCoding/SpaceSnap"}
+          description={"Search through NASA's image database with real time images and info about the image."}
         />
         <PortfolioItem
           src={"/trivia.png"}
@@ -62,6 +69,7 @@ export default function Portfolio() {
           alt={"preview of trivia game homescreen"}
           tech={"Javascript, DOM manipulation, Storybook"}
           link={"https://github.com/JpadillaCoding/WDYKT-Trivia"}
+          description={"Trivia game with random tid-bit information that I know. It's what I'm known for!"}
         />
       </div>
     </main>
