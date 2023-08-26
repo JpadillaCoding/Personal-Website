eb8dfbfc0099039e27344ced96d207da411c2f40
Jose Padilla <jose.apadilla99@gmail.com>
2023-08-26 12:25:02 -0500

    feat: modal items seperated by border

--- app/components/navigation/Navigation.tsx
+++ app/components/navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { use, useState } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./page.module.css";
