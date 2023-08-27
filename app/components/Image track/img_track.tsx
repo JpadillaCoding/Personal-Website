e8f8c559edb3ac32f3c64852914949e226fa38e5
Jose Padilla <jose.apadilla99@gmail.com>
2023-08-27 09:49:19 -0500

    style: grabbing cursor set

--- app/components/Image track/img_track.tsx
+++ app/components/Image track/img_track.tsx
@@ -14,6 +14,7 @@ export default function ImageTrack() {
       };
       const handleDown = (e:MouseEvent) => {
         track.dataset.mouseDownAt = e.clientX.toString();
+        track.classList.add("dragging")
         console.log(e.clientX.toString())
       };
       const handleMove = (e:MouseEvent) => {
@@ -90,7 +91,7 @@ export default function ImageTrack() {
         data-mouse-down-at="0"
         data-prev-percentage="0"
       >
-        <i className={`${styles.double_chevron} ${styles.bounce} ${styles.animated}`}></i>
+        <i className={styles.double_chevron}></i>
         <div className={styles.img_wrapper}>
           <Image
             src="/spaceSnap.png"
